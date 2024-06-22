29b144c8a63c95aec7f85099ab26f82b3d34b4a1
davidbl994 <blazevic.david@hotmail.com>
2024-06-22 19:36:27 +0200

    conftest.py file created driver function that set up Chrome driver and home_url function

---
+++ conftest.py
@@ -0,0 +1,32 @@
+import ssl
+import pytest
+from selenium import webdriver
+import chromedriver_autoinstaller
+from selenium.webdriver.chrome.options import Options
+
+ssl.create_default_https_context = ssl.create_unverified_context
+
+# CONSTANTS
+
+HOME_PAGE = 'https://phptravels.com/demo/'
+
+@pytest.fixture
+def driver():
+    chromedriver_autoinstaller.install()
+
+    # Set up Chrome options for headless mode
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+
+    # Create the Webdriver with the specified
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_window_size(1920, 1080)
+
+    driver.implicitly_wait(5)
+    yield driver
+    driver.quit()
+
+@pytest.fixture
+def home_url():
+    return HOME_PAGE
