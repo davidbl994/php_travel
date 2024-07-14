caf60d44c461635eed505bb4c6b7fa608fa8e056
davidbl994 <blazevic.david@hotmail.com>
2024-07-14 19:25:21 +0200

    Refactor login page test to ensure ‘Remember me’ checkbox remains selected

--- tests/test_login_page/test_ui_ux/test_remember_me_select.py
+++ tests/test_login_page/test_ui_ux/test_remember_me_select.py
@@ -1,18 +1,16 @@
 # Verify that the checkbox Remember me is selected
 import pytest
-from pages.login_page import LoginPage
 
-@pytest.mark.usefixtures("driver", "login_url")
-def test_load_login_page(driver, login_url):
-    login_page = LoginPage(driver)
-    print("Opening login page...")
-    login_page.open_login_page(login_url)
-
-    # Click on the checkbox Remember me
+@pytest.mark.usefixtures("driver", "login_page")
+def test_load_login_page(driver, login_page):
+    """
+    Test to verify that the 'Remember me' checkbox is selectable and remains selected.
+    """
+    # Click on the checkbox 'Remember me'
     print("Clicking on the checkbox 'Remember me'...")
-    #login_page.select_remember_me()
+    login_page.select_remember_me()
 
-    # Check that the checkbox Remember me is selected
+    # Check that the checkbox 'Remember me' is selected
     print("Checking that the checkbox is selected...")
-    assert login_page.is_remember_me_box_selected(), "Checkbox should be selected once it is clicked on it"
-    print("Checkbox Remember me is selected!")
+    assert login_page.is_remember_me_box_selected(), "Checkbox should be selected after clicking on it"
+    print("Checkbox 'Remember me' is selected!")
