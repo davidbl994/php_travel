937fe051238855eb0e2a7591f8621bea8d5bb41c
davidbl994 <blazevic.david@hotmail.com>
2024-07-14 19:29:27 +0200

    Update login page test to verify expected placeholders for username and password fields

--- tests/test_login_page/test_ui_ux/test_expected_placeholders.py
+++ tests/test_login_page/test_ui_ux/test_expected_placeholders.py
@@ -1,13 +1,11 @@
 # Verify that the placeholder texts for username and password fields are correct.
 import pytest
-from pages.login_page import LoginPage
-
-@pytest.mark.usefixtures("driver", "login_url")
-def test_load_login_page(driver, login_url):
-    login_page = LoginPage(driver)
-    print("Opening login page...")
-    login_page.open_login_page(login_url)
-
+@pytest.mark.usefixtures("driver", "login_page")
+def test_load_login_page(driver, login_page):
+    """
+    Test to verify that the placeholder texts for username and password fields are correct.
+    """
+    # Verify placeholder texts
     print("Checking that the placeholder texts for username and password fields are correct...")
     login_page.correct_placeholders()
     print("Placeholder texts for username and password fields are correct!")
