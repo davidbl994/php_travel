dffec6731ad485f5c6aadec0ba0200270c428d4c
davidbl994 <blazevic.david@hotmail.com>
2024-07-14 19:30:48 +0200

    Refactor login page test to confirm correct page title validation

--- tests/test_login_page/test_ui_ux/test_correct_title_login_page.py
+++ tests/test_login_page/test_ui_ux/test_correct_title_login_page.py
@@ -1,13 +1,10 @@
-# Verify that the login page has the correct title.
 import pytest
-from pages.login_page import LoginPage
 
-@pytest.mark.usefixtures("driver", "login_url")
-def test_load_login_page(driver, login_url):
-    login_page = LoginPage(driver)
-    print("Opening login page...")
-    login_page.open_login_page(login_url)
-
+@pytest.mark.usefixtures("driver", "login_page")
+def test_load_login_page(driver, login_page):
+    """
+    Test to verify that the login page has the correct title.
+    """
     # Verify that the login page has the correct title
     print("Checking that the login page has the correct title...")
     login_page.confirm_login_title()
