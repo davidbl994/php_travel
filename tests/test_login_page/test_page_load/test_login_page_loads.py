f54b40f48826d32eb3b24083610f021d1c271357
davidbl994 <blazevic.david@hotmail.com>
2024-07-14 18:59:12 +0200

    Refactoring code - remove login_page block code and adding test description code

--- tests/test_login_page/test_page_load/test_login_page_loads.py
+++ tests/test_login_page/test_page_load/test_login_page_loads.py
@@ -1,13 +1,10 @@
 # Verify that the login page loads successfully
 import pytest
-from pages.login_page import LoginPage
-
 @pytest.mark.usefixtures("driver", "login_url")
-def test_load_login_page(driver, login_url):
-    login_page = LoginPage(driver)
-    print("Opening login page...")
-    login_page.open_login_page(login_url)
-
+def test_load_login_page(driver, login_page):
+    """
+    Test that the login page loads successfully and confirms the presence of the login form.
+    """
     # Check that the login form is present
     print("Checking that the login form is present...")
     assert login_page.login_form_confirm(), "Failed to confirm the login form is present"
