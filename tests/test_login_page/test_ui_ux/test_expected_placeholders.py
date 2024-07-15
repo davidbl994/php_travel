1420b1c100badec9c343b28719b10db0377bfbe7
davidbl994 <blazevic.david@hotmail.com>
2024-07-15 19:33:49 +0200

    Calling logger from the pages/login_page.py and removing comments

--- tests/test_login_page/test_ui_ux/test_expected_placeholders.py
+++ tests/test_login_page/test_ui_ux/test_expected_placeholders.py
@@ -1,11 +1,10 @@
-# Verify that the placeholder texts for username and password fields are correct.
 import pytest
+from pages.login_page import logger
 @pytest.mark.usefixtures("driver", "login_page")
 def test_load_login_page(driver, login_page):
     """
     Test to verify that the placeholder texts for username and password fields are correct.
     """
-    # Verify placeholder texts
-    print("Checking that the placeholder texts for username and password fields are correct...")
+    logger.info("Checking that the placeholder texts for username and password fields are correct...")
     login_page.correct_placeholders()
-    print("Placeholder texts for username and password fields are correct!")
+    logger.info("Placeholder texts for username and password fields are correct!")
