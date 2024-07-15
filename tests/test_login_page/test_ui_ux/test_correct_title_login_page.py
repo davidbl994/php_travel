31c293ba775186c18766f857b03d19efc0fb5fe0
davidbl994 <blazevic.david@hotmail.com>
2024-07-15 19:28:42 +0200

    Calling logger from the pages/login_page.py and removing a comment

--- tests/test_login_page/test_ui_ux/test_correct_title_login_page.py
+++ tests/test_login_page/test_ui_ux/test_correct_title_login_page.py
@@ -6,7 +6,6 @@ def test_load_login_page(driver, login_page):
     """
     Test to verify that the login page has the correct title.
     """
-    # Verify that the login page has the correct title
     logger.info("Checking that the login page has the correct title...")
     login_page.confirm_login_title()
     logger.info("Login page has the correct title!")
