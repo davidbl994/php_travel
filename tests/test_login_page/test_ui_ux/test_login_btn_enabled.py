4a645781ec30393fb2064558a5eea00ce6fa735a
davidbl994 <blazevic.david@hotmail.com>
2024-07-15 19:34:55 +0200

    Calling logger from the pages/login_page.py and removing comments

--- tests/test_login_page/test_ui_ux/test_login_btn_enabled.py
+++ tests/test_login_page/test_ui_ux/test_login_btn_enabled.py
@@ -1,13 +1,11 @@
-# Verify that the login button is enabled when the page loads.
-
 import pytest
+from pages.login_page import logger
 
 @pytest.mark.usefixtures("driver", "login_page")
 def test_load_login_page(driver, login_page):
     """
     Test to verify that the login button is enabled when the login page loads.
     """
-    # Verify login button is enabled
-    print("Verifying that the login button is enabled...")
+    logger.info("Verifying that the login button is enabled...")
     assert login_page.is_login_button_enabled(), "Login button should be enabled when the page loads"
-    print("Login button is enabled!")
+    logger.info("Login button is enabled!")
