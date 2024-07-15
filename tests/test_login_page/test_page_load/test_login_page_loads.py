2794165708b9d1c8539b278f451d63c37f93a38f
davidbl994 <blazevic.david@hotmail.com>
2024-07-15 19:08:21 +0200

    Removing print statements and using logger instead for better log management. Also, removed comments

--- tests/test_login_page/test_page_load/test_login_page_loads.py
+++ tests/test_login_page/test_page_load/test_login_page_loads.py
@@ -1,11 +1,10 @@
-# Verify that the login page loads successfully
 import pytest
-@pytest.mark.usefixtures("driver", "login_url")
+from pages.login_page import logger
+@pytest.mark.usefixtures("driver", "login_page")
 def test_load_login_page(driver, login_page):
     """
     Test that the login page loads successfully and confirms the presence of the login form.
     """
-    # Check that the login form is present
-    print("Checking that the login form is present...")
-    assert login_page.login_form_confirm(), "Failed to confirm the login form is present"
-    print("Login form is successfully loaded!")
+    logger.info("Checking that the login form is present...")
+    assert login_page.login_form_confirm(), "Login form is not present on the login page."
+    logger.info("Login form is successfully loaded!")
