226c3791a2a864af3ad3f0fbd168e6c0738cde4b
davidbl994 <blazevic.david@hotmail.com>
2024-07-15 19:28:31 +0200

    Calling logger from the pages/login_page.py and removing a comment

--- tests/test_login_page/test_ui_ux/test_correct_title_login_page.py
+++ tests/test_login_page/test_ui_ux/test_correct_title_login_page.py
@@ -1,4 +1,5 @@
 import pytest
+from pages.login_page import logger
 
 @pytest.mark.usefixtures("driver", "login_page")
 def test_load_login_page(driver, login_page):
@@ -6,6 +7,6 @@ def test_load_login_page(driver, login_page):
     Test to verify that the login page has the correct title.
     """
     # Verify that the login page has the correct title
-    print("Checking that the login page has the correct title...")
+    logger.info("Checking that the login page has the correct title...")
     login_page.confirm_login_title()
-    print("Login page has the correct title!")
+    logger.info("Login page has the correct title!")
