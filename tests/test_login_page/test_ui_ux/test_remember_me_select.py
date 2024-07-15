80cad19940f45cdc720a78973c42c11668b87cad
davidbl994 <blazevic.david@hotmail.com>
2024-07-15 19:36:31 +0200

    Calling logger from the pages/login_page.py and removing comments

--- tests/test_login_page/test_ui_ux/test_remember_me_select.py
+++ tests/test_login_page/test_ui_ux/test_remember_me_select.py
@@ -1,16 +1,14 @@
-# Verify that the checkbox Remember me is selected
 import pytest
+from pages.login_page import logger
 
 @pytest.mark.usefixtures("driver", "login_page")
 def test_load_login_page(driver, login_page):
     """
     Test to verify that the 'Remember me' checkbox is selectable and remains selected.
     """
-    # Click on the checkbox 'Remember me'
-    print("Clicking on the checkbox 'Remember me'...")
+    logger.info("Clicking on the checkbox 'Remember me'...")
     login_page.select_remember_me()
 
-    # Check that the checkbox 'Remember me' is selected
-    print("Checking that the checkbox is selected...")
+    logger.info("Checking that the checkbox is selected...")
     assert login_page.is_remember_me_box_selected(), "Checkbox should be selected after clicking on it"
-    print("Checkbox 'Remember me' is selected!")
+    logger.info("Checkbox 'Remember me' is selected!")
