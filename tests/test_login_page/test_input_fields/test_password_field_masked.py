9587c4c326eb7a49dfeffaf0d58f8229def4978b
davidbl994 <blazevic.david@hotmail.com>
2024-07-17 20:06:47 +0200

    Test that verifies input password field is masked

---
+++ tests/test_login_page/test_input_fields/test_password_field_masked.py
@@ -0,0 +1,23 @@
+import pytest
+from pages.login_page import logger
+from utils.config import PASSWORD
+from utils.locators import Login
+
+@pytest.mark.usefixtures("driver")
+def test_input_fields_cleared_on_refresh(driver, login_page):
+
+    logger.info("Inserting a password...")
+    login_page.insert_password(PASSWORD)
+    logger.info("Password inserted!")
+
+    password_field = driver.find_element(*Login.PASSWORD)
+
+    logger.info("Verifying password field...")
+    assert password_field.get_attribute('type') == 'password', "Password field is not masked"
+    logger.info("Password field is of type 'password'")
+
+    entered_password = PASSWORD
+    actual_value = password_field.get_attribute('value')
+    assert actual_value == entered_password, "Password input does not match expected value"
+    assert '*' not in actual_value, "Password field is displaying input characters"
+    logger.info("Password field is not displaying input characters!")
