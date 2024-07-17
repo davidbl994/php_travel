e2ee5988cff25c04314644151e12800d17a6f499
davidbl994 <blazevic.david@hotmail.com>
2024-07-17 20:25:41 +0200

    Test that verifies that the session cookie is created upon successful login

---
+++ tests/test_login_page/test_security/test_session_cookie_creation.py
@@ -0,0 +1,14 @@
+from pages.dashboard_page import DashboardPage
+from utils.config import USERNAME, PASSWORD
+
+
+def test_session_cookie_creation(driver, login_page):
+
+    login_page.insert_email(USERNAME)
+    login_page.insert_password(PASSWORD)
+    login_page.click_login_btn()
+
+    dashboard_page = DashboardPage(driver)
+    dashboard_page.welcome_back_msg()
+
+    dashboard_page.cookie_session_confirm()
