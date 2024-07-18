4a3d5ab7d1adde11e8e7beb31910bc84e8ad7821
davidbl994 <blazevic.david@hotmail.com>
2024-07-18 17:59:11 +0200

    Removing deprecated logger.info comments

--- tests/test_login_page/test_input_fields/test_fields_cleared_after_refresh.py
+++ tests/test_login_page/test_input_fields/test_fields_cleared_after_refresh.py
@@ -1,22 +1,16 @@
 import pytest
-from pages.login_page import logger
 from utils.config import USERNAME, PASSWORD
+from pages.login_page import logger
 from utils.locators import Login
 
 @pytest.mark.usefixtures("driver")
 def test_input_fields_cleared_on_refresh(driver, login_page):
 
-    logger.info("Inserting email...")
     login_page.insert_email(USERNAME)
-    logger.info("Email inserted!")
-
-    logger.info("Inserting password...")
     login_page.insert_password(PASSWORD)
-    logger.info("Password inserted")
 
-    logger.info("Refreshing web page...")
     driver.refresh()
-    logger.info("Web page refreshed!")
+    print("Web page refreshed!")
 
     email_field = driver.find_element(*Login.EMAIL_ADDRESS)
     password_field = driver.find_element(*Login.PASSWORD)
@@ -28,4 +22,3 @@ def test_input_fields_cleared_on_refresh(driver, login_page):
     logger.info("Checking password field is cleared...")
     assert password_field.get_attribute('value') == "", "Password field is not cleared"
     logger.info("Password field is cleared!")
-
