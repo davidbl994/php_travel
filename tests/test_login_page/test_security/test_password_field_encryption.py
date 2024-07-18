3b86cb94e5f716dd084eaf565a62cdf40324ac98
davidbl994 <blazevic.david@hotmail.com>
2024-07-18 19:03:28 +0200

    Adding logger to describe test steps execution

--- tests/test_login_page/test_security/test_password_field_encryption.py
+++ tests/test_login_page/test_security/test_password_field_encryption.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.by import By
+from pages.login_page import logger
 
 def test_password_field_encryption(driver, login_page):
 
     password_field_locator = driver.find_element(By.NAME, 'password')
+    logger.info("Checking is password securely stored in the browser...")
     assert password_field_locator.get_attribute("type") == "password", "Password is not stored securely in the browser"
+    logger.info("Password is securely stored in the browser!")
