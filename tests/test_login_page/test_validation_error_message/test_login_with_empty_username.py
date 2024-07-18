bfccedc0c79c1da53e40d3f059167710e14b5e46
davidbl994 <blazevic.david@hotmail.com>
2024-07-18 18:54:26 +0200

    Removing logger from the test

--- tests/test_login_page/test_validation_error_message/test_login_with_empty_username.py
+++ tests/test_login_page/test_validation_error_message/test_login_with_empty_username.py
@@ -1,14 +1,9 @@
-from pages.login_page import logger
 from utils.config import EMPTY_USERNAME, PASSWORD
 
 def test_logout_user(driver, login_page, search_url):
-    logger.info("Inserting mail...")
     login_page.insert_email(EMPTY_USERNAME)
-    logger.info("Inserting password...")
     login_page.insert_password(PASSWORD)
-    logger.info("Selecting remember me checkbox...")
     login_page.select_remember_me()
-    logger.info("Clicking login button...")
     login_page.click_login_btn()
 
     login_page.get_error_message(driver)
