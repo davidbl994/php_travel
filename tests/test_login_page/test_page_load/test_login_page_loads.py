776a3cc7c9ac98be1c278a1e20518cf4784dfe4e
davidbl994 <blazevic.david@hotmail.com>
2024-07-18 18:43:36 +0200

    Removing logger from the test

--- tests/test_login_page/test_page_load/test_login_page_loads.py
+++ tests/test_login_page/test_page_load/test_login_page_loads.py
@@ -1,10 +1,5 @@
 import pytest
-from pages.login_page import logger
 @pytest.mark.usefixtures("driver", "login_page")
 def test_load_login_page(driver, login_page):
-    """
-    Test that the login page loads successfully and confirms the presence of the login form.
-    """
-    logger.info("Checking that the login form is present...")
+
     assert login_page.login_form_confirm(), "Login form is not present on the login page."
-    logger.info("Login form is successfully loaded!")
