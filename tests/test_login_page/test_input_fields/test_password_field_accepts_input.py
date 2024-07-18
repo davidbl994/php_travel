b5a44a0827564f5157cd6f591c7b40123f447603
davidbl994 <blazevic.david@hotmail.com>
2024-07-18 18:23:29 +0200

    Removing logger from the test and adding print statement

--- tests/test_login_page/test_input_fields/test_password_field_accepts_input.py
+++ tests/test_login_page/test_input_fields/test_password_field_accepts_input.py
@@ -1,16 +1,13 @@
 import pytest
-from pages.login_page import logger
 from utils.config import PASSWORD
 from utils.locators import Login
 
 @pytest.mark.usefixtures("driver")
 def test_input_fields_cleared_on_refresh(driver, login_page):
 
-    logger.info("Inserting a password...")
     login_page.insert_password(PASSWORD)
-    logger.info("Password inserted!")
 
     password_field = driver.find_element(*Login.PASSWORD)
-    logger.info("Checking the expected input...")
+    print("Checking the expected input...")
     assert password_field.get_attribute('value') == PASSWORD, "Password field does not contain the expected input"
-    logger.info("Password field contains the expected input!")
+    print("Password field contains the expected input!")
