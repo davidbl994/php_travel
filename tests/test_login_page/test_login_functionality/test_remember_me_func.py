a23a999c0cc586be5af49a7e77c58bc423a73f0e
davidbl994 <blazevic.david@hotmail.com>
2024-07-18 18:31:36 +0200

    Removing logger from the test and adding print statements

--- tests/test_login_page/test_login_functionality/test_remember_me_func.py
+++ tests/test_login_page/test_login_functionality/test_remember_me_func.py
@@ -1,22 +1,17 @@
 import pytest
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
-from pages.login_page import logger
 from utils.config import USERNAME, PASSWORD
 
 @pytest.mark.xfail
 def test_remember_me_functionality(driver, login_page, login_url, dashboard_url):
 
-    logger.info("Inserting mail...")
     login_page.insert_email(USERNAME)
-    logger.info("Inserting password...")
     login_page.insert_password(PASSWORD)
-    logger.info("Selecting remember me checkbox...")
     login_page.select_remember_me()
-    logger.info("Clicking login button...")
     login_page.click_login_btn()
 
-    logger.info("Verifying that we are logged in...")
+    print("Verifying that we are logged in...")
     cookies = driver.get_cookies()
 
     dashboard_page = DashboardPage(driver)
@@ -35,6 +30,6 @@ def test_remember_me_functionality(driver, login_page, login_url, dashboard_url)
         driver.add_cookie(cookie)
     driver.refresh()
 
-    logger.info("Verifying that we are still logged in...")
+    print("Verifying that we are still logged in...")
     dashboard_page = DashboardPage(driver)
     assert dashboard_page.is_logged_in(), "User was not remembered after closing and reopening the browser!"
